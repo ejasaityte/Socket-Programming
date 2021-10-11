ecddfc97d6bc464e50557c9aa9cd0eceb0c7a757
matt-bain <mbain@dundee.ac.uk>
2021-10-12 00:41:28 +0100

    added comments to server.py

--- server.py
+++ server.py
@@ -1,23 +1,28 @@
 import socket
 import threading
 
-HEADER = 1024
-PORT = 6667
-SERVER = "::1"
-ADDR = (SERVER, PORT)
+HEADER = 1024 #standard accepted header size in bytes. saves faffing with numbers in the code
+PORT = 6667 #hard coded port
+SERVER = "::1" #hard coded IP
+ADDR = (SERVER, PORT) #tuple for connecting to server
 
+# server boot up
+# need to add error handling for a failed connection (should be fine tho)
 server = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 server.bind(ADDR)
 server.listen()
 print(f"[LISTENING] Server is listening on {SERVER}:{PORT}")
 
-clients = []
-nicks = []
+clients = [] #stores the connection instances for all clients 
+nicks = [] #stores the nicknames of all clients
 
+# send message to all clients
+# no idea if this is neccessary but nice to have
 def broadcast(message):
-	for client in clients:
+	for client in clients: # messy implementation but it does the job
 		client.send(message)
 
+# handle the clients messages that are sent
 def handle_client(client):
 	while True:
 		i = clients.index(client)
@@ -29,29 +34,43 @@ def handle_client(client):
 			client.close()
 			break
 
+# handle first communication from clients
 def recieve():
 	while True:
-		client, addr = server.accept()
+		client, addr = server.accept() # open connection (error handling not present yet)
 		print(f"Connected with {str(addr)}")
 
+		# for the first 2 recieved comms
 		for x in range(2):
-			rcv = client.recv(HEADER).decode('ascii')
-			first_chars = rcv[0:4]
-			if "NICK" in first_chars:
-				subs = rcv.split('\n')
-				nick = subs[0][5: ]
+			rcv = client.recv(HEADER).decode('ascii') # decode the bytes into words
+
+			first_chars = rcv[0:4] # take the first 4 characters of the message
+								   # this will be where the command is e.g. NICK,
+								   # JOIN, etc. if some wise guy decides to call them-
+								   # -self "NICK" to mess up the system, this counters that
+
+			if "NICK" in first_chars: # handling NICK command
+
+				subs = rcv.split('\n') # NICK command also contains the USER part
+									   # but we don't want that, so we split around the
+									   # new line
+				nick = subs[0][5: ] # set the nickname to everything following "NICK "
 				print(nick)
 				nicks.append(nick)
 				joinmsg_serv = "{0} joined the server!\r\n"
 				joinmsg_client = "Welcome to the server {0}!\r\n"
 				broadcast(joinmsg_serv.format(nick).encode('ascii'))
 				client.send(joinmsg_client.format(nick).encode('ascii'))
-			elif "JOIN" in first_chars:
+
+			elif "JOIN" in first_chars: # handling JOIN command
+				# TODO: handle this
 				break
 		
-		clients.append(client)
+		clients.append(client) # add client instance to the array
+		nicks.append(nick) # add nickname to array
 
-		thread = threading.Thread(target = handle_client, args = (client,))
+		# start a thread for handling the client comms
+		thread = threading.Thread(target = handle_client, args = (client,)) 
 		thread.start()
 
 print("[STARTING] Server is starting...")
